78dac8ab90c91b18ed92e95d80cabd142d1d8950
pawel <spawel114@gmail.com>
2020-12-31 15:22:48 +0100

    Modified way of connecting with database.

---
+++ modules/uwsgi-flask/app/mariadb_dao.py
@@ -0,0 +1,43 @@
+import time
+import mysql.connector as mariadb
+import os
+
+PASSWORD = "MYSQL_ROOT_PASSWORD"
+
+class MariaDBDAO:
+
+    def connect(self, host, user, password):
+        try:
+            db = mariadb.connect(host=host, user=user, password=password)
+            sql = db.cursor(buffered=True)
+            sql.execute("USE mysql")
+            sql.execute("SELECT 1")
+            sql.fetchall()
+            return db
+        except Exception as err:
+            print(f"Error while connecting with MariaDB: {err}")
+            time.sleep(3)
+            return None
+    
+    def choose_database(self, database):
+        try:
+            sql = self.db.cursor(buffered=True)
+            sql.execute(f"USE {database}")
+            sql.execute("SELECT 1")
+            sql.fetchall()
+            return sql
+        except Exception as err:
+            print(f"Error while choosing DB with MariaDB: {err}")
+            print(f"Initiating database ...")
+            import app.init_mariadb
+            time.sleep(3)
+            return None
+
+    def __init__(self, host):
+        self.db = None
+        self.sql = None
+        while self.db is None:
+            self.db = self.connect(host, "root", os.environ.get(PASSWORD))
+        while self.sql is None:
+            self.sql = self.choose_database("db")
+        print("Connected to MariaDB.")
